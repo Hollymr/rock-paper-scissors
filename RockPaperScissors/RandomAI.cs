2969a80bd2359e51e16f4fe4fe2eee6335f893f8
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-01 11:44:07 -0400

    RandomAI 1 complete

---
+++ RockPaperScissors/RandomAI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class RandomAI : IPlayer
+    {
+        Random random = new Random(); //outside to only generate one random generator, otherwise will keep creating
+
+        public int NextMove()
+        {
+            // 0: Rock, 1: Paper, 2: Scissors 
+            return random.Next(0, 3);
+        }
+
+        public void SaveResult(int myMove, int otherMove)
+        {
+            // do nothing method
+            // going to choose random move so doesn't need to remember pervious move
+        }
+    }
+}
